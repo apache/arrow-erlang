5ece60b8c9fb9552450d9f6dd0ea18d69e7c9c81
Benjamin Philip <benjamin.philip495@gmail.com>
2026-07-19 18:22:30 +0530

    Migrate arrow_type

--- src/arrow_type.erl
+++ src/arrow_type.erl
@@ -15,123 +15,94 @@
 % specific language governing permissions and limitations
 % under the License.
 
-%%%-----------------------------------------------------------------------------
-%% @doc Provides representation for as well as functions for working with Arrow
-%% specific datatypes.
-%%
-%% This module provides a longhand (a 2 tuple) as well as a shorthand (an atom)
-%% represenations for representing a primitive types. Additionally it represents
-%% Nested Types (i.e. Lists) as well as Native Types (i.e. Erlang Native Types
-%% that are supported by this package).
-%%
-%% Generally, when we say an array A has type X, we mean that each element in A
-%% has type X. Thus, an array of type of `Int8' would look like `[1, 2, 3]', but
-%% an array of type `List<Int8>' would look like `[[1, 2], [3]]'.
-%%
-%% == Primitive Types ==
-%%
-%% A Primitive Type is any type that refers to a single value (as opposed to a
-%% list of values). Currently it includes Booleans, Signed Integers, Unsigned
-%% Integers, Floats and Binaries. Both a longhand and shorthand syntax had been
-%% provided to initiate a type.
-%%
-%% The following is a comprehensive list of all supported primitive types:
-%%
-%% <ol>
-%%  <li>Booleans</li>
-%%  <li>Signed Integers: Includes `Int 8', `Int 16', `Int 32' and `Int 64'</li>
-%%  <li>
-%%      Unsigned Integers: Includes `UInt 8', `UInt 16', `UInt 32' and `UInt 64'
-%%  </li>
-%%  <li>Floats: Includes `Float 16', `Float 32' and `Float 64'</li>
-%%  <li>Binaries</li>
-%% </ol>
-%%
-%% === Shorthand Syntax ===
-%%
-%% In the shorthand syntax, you can just use a single atom to refer to a type,
-%% such that the atom is `<name><size>'. The name is a single letter that refers
-%% to the type, and the size is that type's size in bits. Take the example of
-%% `Int 8'. Its name is `s', and size is `8'. Thus in shorthand, it is `s8'. In
-%% the case of datatypes without variations, such as Booleans and Binaries, they
-%% are represented by just their datatype. Do note that internally the longhand
-%% syntax is used.
-%%
-%% Here you can find the name of each type:
-%%
-%% <table border="1">
-%%  <tr>
-%%      <td>Type</td>
-%%      <td>Name</td>
-%%      <td>Example Atom</td>
-%%  </tr>
-%%  <tr>
-%%      <td>Booleans</td>
-%%      <td>N/A</td>
-%%      <td>`bool'</td>
-%%  </tr>
-%%  <tr>
-%%      <td>Signed Integer</td>
-%%      <td>`s'</td>
-%%      <td>`s64'</td>
-%%  </tr>
-%%  <tr>
-%%      <td>Unsigned Integer</td>
-%%      <td>`u'</td>
-%%      <td>`u16'</td>
-%%  </tr>
-%%  <tr>
-%%      <td>Float</td>
-%%      <td>`f'</td>
-%%      <td>`f32'</td>
-%%  </tr>
-%%  <tr>
-%%      <td>Binaries</td>
-%%      <td>N/A</td>
-%%      <td>`bin'</td>
-%%  </tr>
-%% </table>
-%%
-%% Types that are shorthand are under the `t:arrow_shorthand_type()' and
-%% `arrow_short_*()' types.
-%%
-%% === Longhand Syntax ===
-%%
-%% In longhand syntax, you use a 2 tuple to to refer to the type, such that the
-%% tuple is `{Name, Size}'. Thus, `Int 8' would be represented as `{s, 8}'. Do
-%% note that in the case or Booleans and Binaries their `Size' is `undefined', as
-%% the have variable or undefined size.
-%%
-%% Types that are longhand are under the `t:arrow_longhand_type()' and
-%% `arrow_long_*()' types.
-%%
-%% == Nested Type ==
-%%
-%% A Nested Type is any data structure that supports nesting. This can include
-%% Lists, FixedLists, Maps, Structs and more, but is curently limited to
-%% FixedLists. Nested Types are represented using a 3 tuple: `{NestedType, Type,
-%% Length}'. `NestedType' refers to the data structure that is nesting your
-%% primitive type. `Type' refers to the type that is being nested in the data
-%% structure. Do note that `Type' can be a Primitive Type, or another Nested
-%% Type. Both the shorthand and the longhand syntax can be used or Primitive
-%% Types.`Length' refers to the length of each element, and will only be
-%% required to represent FixedLists.
-%%
-%% It is important to remember while nesting that the type refers to the type of
-%% each element in an array. A list of `Int8' is 2 dimensional, and a list of
-%% `List<Int8>' is 3 dimensional.
-%%
-%% == Native Type ==
-%%
-%% A Native Type is any Erlang Datatype that can be serialized by `arrow',
-%% and is represented by the type `t:native_type()'. It currently only includes
-%% values that are primitive in nature such as booleans, integers, floats,
-%% binaries, and additionally the atoms `undefined' and `nil' to represent a
-%% `NULL' value.
-%%
-%% @end
-%%%-----------------------------------------------------------------------------
 -module(arrow_type).
+-moduledoc """
+Provides representation for as well as functions for working with Arrow
+specific datatypes.
+
+This module provides a longhand (a 2 tuple) as well as a shorthand (an atom)
+represenations for representing a primitive types. Additionally it represents
+Nested Types (i.e. Lists) as well as Native Types (i.e. Erlang Native Types
+that are supported by this package).
+
+Generally, when we say an array A has type X, we mean that each element in A
+has type X. Thus, an array of type of `Int8` would look like `[1, 2, 3]`, but
+an array of type `List<Int8>` would look like `[[1, 2], [3]]`.
+
+## Primitive Types
+
+A Primitive Type is any type that refers to a single value (as opposed to a
+list of values). Currently it includes Booleans, Signed Integers, Unsigned
+Integers, Floats and Binaries. Both a longhand and shorthand syntax had been
+provided to initiate a type.
+
+The following is a comprehensive list of all supported primitive types:
+
+1.  Booleans
+2.  Signed Integers: Includes `Int 8`, `Int 16`, `Int 32` and
+    `Int 64`
+3.  Unsigned Integers: Includes `UInt 8`, `UInt 16`, `UInt 32` and
+    `UInt 64`
+4.  Floats: Includes `Float 16`, `Float 32` and `Float 64`
+5.  Binaries
+
+### Shorthand Syntax
+
+In the shorthand syntax, you can just use a single atom to refer to a type,
+such that the atom is `<name><size>`. The name is a single letter that refers
+to the type, and the size is that type's size in bits. Take the example of
+`Int 8`. Its name is `s`, and size is `8`. Thus in shorthand, it is `s8`. In
+the case of datatypes without variations, such as Booleans and Binaries, they
+are represented by just their datatype. Do note that internally the longhand
+syntax is used.
+
+Here you can find the name of each table:
+
+| Type             | Name  | Example Atom |
+|------------------|-------|--------------|
+| Booleans         | N/A   | `bool`       |
+| Signed Integer   | `s`   | `s64`        |
+| Unsigned Integer | `u `  | `u16 `       |
+| Float            | `f `  | `f32 `       |
+| Binaries         | N/A   | `bin `       |
+
+Types that are shorthand are under the `t:arrow_shorthand_type()` and
+`arrow_short_*()` types.
+
+### Longhand Syntax
+
+In longhand syntax, you use a 2 tuple to to refer to the type, such that the
+tuple is `{Name, Size}`. Thus, `Int 8` would be represented as `{s, 8}`. Do
+note that in the case or Booleans and Binaries their `Size` is `undefined`, as
+the have variable or undefined size.
+
+Types that are longhand are under the `t:arrow_longhand_type()` and
+`arrow_long_*()` types.
+
+### Type
+
+A Nested Type is any data structure that supports nesting. This can include
+Lists, FixedLists, Maps, Structs and more, but is curently limited to
+FixedLists. Nested Types are represented using a 3 tuple: `{NestedType, Type,
+Length}`. `NestedType` refers to the data structure that is nesting your
+primitive type. `Type` refers to the type that is being nested in the data
+structure. Do note that `Type` can be a Primitive Type, or another Nested
+Type. Both the shorthand and the longhand syntax can be used or Primitive
+Types.`Length` refers to the length of each element, and will only be
+required to represent FixedLists.
+
+It is important to remember while nesting that the type refers to the type of
+each element in an array. A list of `Int8` is 2 dimensional, and a list of
+`List<Int8>` is 3 dimensional.
+
+## Native Type
+
+A Native Type is any Erlang Datatype that can be serialized by `arrow`,
+and is represented by the type `t:native_type()`. It currently only includes
+values that are primitive in nature such as booleans, integers, floats,
+binaries, and additionally the atoms `undefined` and `nil` to represent a
+`NULL` value.
+""".
 -export([normalize/1, bit_length/1, byte_length/1, serialize/2]).
 -export_type([
     arrow_type/0,
@@ -354,7 +325,9 @@ normalize(_Type) ->
     erlang:error(badarg).
 
 -spec bit_length(Type :: arrow_type()) -> Length :: pos_integer() | undefined.
-%% @doc Returns the size of the type in bits.
+-doc """
+Returns the size of the type in bits.
+""".
 bit_length(Type) when is_atom(Type) ->
     bit_length(normalize(Type));
 bit_length({Type, Size}) when (Type =:= s) orelse (Type =:= u) orelse (Type =:= f) ->
@@ -367,7 +340,9 @@ bit_length(Type) when tuple_size(Type) =:= 3 ->
     erlang:error(badarg).
 
 -spec byte_length(Type :: arrow_type()) -> Length :: pos_integer() | undefined.
-%% @doc Returns the size of the type in bytes.
+-doc """
+Returns the size of the type in bytes.
+""".
 byte_length(Type) when is_atom(Type) ->
     byte_length(normalize(Type));
 byte_length(Type) when tuple_size(Type) =:= 3 ->
